2d6512f60f68c84626344d9786c46de94665e9d0
Staff <staffan.solin@gmail.com>
2024-08-17 20:03:47 +0200

    finishing up most functionality

---
+++ time-vs-salary/src/app/components/SliderExtended.tsx
@@ -0,0 +1,47 @@
+import { Close, EditOutlined } from "@mui/icons-material";
+import { IconButton, Slider, SliderProps, TextField } from "@mui/material";
+import React, { ChangeEvent } from "react";
+
+type SliderExtendedProps = {
+  text: string;
+  toolipLabel?: string;
+  textfieldValue: number;
+  showTextfield: boolean;
+  sliderSettings: SliderProps;
+  onTextfieldChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onToggleTextfiled: () => void;
+};
+
+function SliderExtended(props: SliderExtendedProps) {
+  return (
+    <div>
+      <div className="flex gap-1 ">
+        <div className="flex gap-1 align-middle items-center ">
+          <label htmlFor="salary">{props.text}</label>
+        </div>
+        <span className="flex gap-1 opacity-65 text-lg">
+          <IconButton onClick={() => props.onToggleTextfiled()}>
+            {props.showTextfield ? (
+              <Close></Close>
+            ) : (
+              <EditOutlined></EditOutlined>
+            )}
+          </IconButton>
+          {props.showTextfield && (
+            <TextField
+              size="small"
+              placeholder={props.text}
+              sx={{ width: "150px" }}
+              onChange={props.onTextfieldChange}
+              value={props.textfieldValue}
+              type="number"
+            ></TextField>
+          )}
+        </span>
+      </div>
+      <Slider {...props.sliderSettings} />
+    </div>
+  );
+}
+
+export default SliderExtended;
--- time-vs-salary/src/app/page.tsx
+++ time-vs-salary/src/app/page.tsx
@@ -1,78 +1,162 @@
 "use client";
-import { Slider } from "@mui/material";
+import { SliderProps } from "@mui/material";
 import { ChangeEvent, useState } from "react";
+import { AccessTime } from "@mui/icons-material";
+import SliderExtended from "./components/SliderExtended";
+
+const MAX_SALARY = 50000;
+const MAX_HOURS = 200;
+const MAX_PRICE = 1000;
 
 export default function Home() {
-  const [salary, setSalary] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [price, setPrice] = useState(0);
+  const [salary, setSalary] = useState(25000);
+  const [hours, setHours] = useState(160);
+  const [price, setPrice] = useState(500);
 
-  function convertDecimalToHoursMinutes(decimalHours: number): string {
+  const [showSalaryTextfield, setShowSalaryTextfield] = useState(false);
+  const [showHoursTextfield, setShowHoursTextfield] = useState(false);
+  const [showPriceTextfield, setShowPriceTextfield] = useState(false);
+
+  function convertDecimalTimeToHMS(decimalHours: number): string {
     const hours = Math.floor(decimalHours);
-    const minutes = Math.round((decimalHours - hours) * 60);
-    return `${hours} hours, ${minutes} minutes`;
+    const minutes = Math.floor((decimalHours * 60) % 60);
+    const seconds = Math.floor((decimalHours * 3600) % 60);
+
+    let hoursString = "";
+    let minutesString = `${minutes} minut${minutes === 1 ? "" : "ar"}`;
+    let secondsString = "";
+
+    if (hours !== 0) {
+      hoursString += `${hours} timm${hours === 1 ? "e" : "ar"}, `;
+    }
+    if (hours === 0) {
+      secondsString += `, ${seconds} sekund${seconds === 1 ? "" : "er"} `;
+    }
+
+    return `${hoursString}${minutesString}${secondsString}`;
   }
 
   const payPerHour = salary / hours;
   const timeInHours = price / payPerHour;
-  const result = convertDecimalToHoursMinutes(timeInHours);
+  const result = convertDecimalTimeToHMS(timeInHours);
 
-  function updateSalary(value: number) {
-    setSalary(value);
-    return `${value}°C`;
+  function updateSalary(event: Event) {
+    const target = event.target as HTMLInputElement;
+    setSalary(+target.value);
   }
 
-  function updateHours(value: number) {
-    setHours(value);
-    return `${value}°C`;
+  function updateHours(event: Event) {
+    const target = event.target as HTMLInputElement;
+    setHours(+target.value);
   }
 
-  function updatePrice(value: number) {
-    setPrice(value);
-    return `${value}°C`;
+  function updatePrice(event: Event) {
+    const target = event.target as HTMLInputElement;
+    setPrice(+target.value);
+  }
+
+  const salarySliderSetting: SliderProps = {
+    "aria-label": "Always visible",
+    onChange: updateSalary,
+    value: salary,
+    step: 100,
+    valueLabelDisplay:
+      salary > MAX_SALARY || showSalaryTextfield ? "off" : "on",
+    max: MAX_SALARY,
+  };
+
+  const hoursSliderSetting: SliderProps = {
+    "aria-label": "Always visible",
+    onChange: updateHours,
+    value: hours,
+    step: 1,
+    valueLabelDisplay: hours > MAX_HOURS || showHoursTextfield ? "off" : "on",
+    max: MAX_HOURS,
+  };
+
+  const priceSliderSetting: SliderProps = {
+    "aria-label": "Always visible",
+    onChange: updatePrice,
+    value: price,
+    step: 1,
+    valueLabelDisplay: price > MAX_PRICE || showPriceTextfield ? "off" : "on",
+    max: MAX_PRICE,
+  };
+
+  function onSalaryChange(event: ChangeEvent<HTMLInputElement>) {
+    setSalary(+event.target.value);
+  }
+
+  function onPriceChange(event: ChangeEvent<HTMLInputElement>) {
+    setPrice(+event.target.value);
+  }
+
+  function onHoursChange(event: ChangeEvent<HTMLInputElement>) {
+    setHours(+event.target.value);
+  }
+
+  function onSalaryTextfieldUpdate() {
+    if (showSalaryTextfield && salary > MAX_SALARY) {
+      setSalary(MAX_SALARY);
+    }
+    setShowSalaryTextfield(!showSalaryTextfield);
+  }
+
+  function onPriceTextfieldUpdate() {
+    if (showPriceTextfield && price > MAX_PRICE) {
+      setPrice(MAX_PRICE);
+    }
+    setShowPriceTextfield(!showPriceTextfield);
+  }
+
+  function onHoursTextfieldUpdate() {
+    if (showHoursTextfield && hours > MAX_HOURS) {
+      setHours(MAX_HOURS);
+    }
+    setShowHoursTextfield(!showHoursTextfield);
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center  p-24 gap-4">
-      <h1>Time vs Salary</h1>
-      <div className="flex flex-col gap-2">
+    <main className="flex min-h-screen flex-col items-center pt-[5vh] gap-4">
+      <header className="flex items-center gap-2">
+        <div className="flex text-[30px] sm:text-[40px] xl:text-[45px]">
+          <AccessTime fontSize="inherit"></AccessTime>
+        </div>
+        <h1 className="font-bold text-xl sm:text-2xl xl:text-3xl">
+          Hur mycket arbetstid kan du spara?
+        </h1>
+      </header>
+      <div className="flex flex-col gap-2 w-full p-8 max-w-2xl rounded overflow-hidden shadow-lg">
         <div className="flex flex-col gap-3">
-          <div className="">
-            <label htmlFor="salary">Monthly salary (after Tax)</label>
-            <Slider
-              aria-label="Always visible"
-              defaultValue={20000}
-              getAriaValueText={updateSalary}
-              step={100}
-              valueLabelDisplay="on"
-              max={50000}
-            />
-          </div>
+          <p className="font-bold text-2xl text-center">
+            {price && salary && hours ? result : 0}
+          </p>
         </div>
-        <div className="">
-          <label htmlFor="hours">Hour working per month</label>
-          <Slider
-            aria-label="Always visible"
-            defaultValue={169}
-            getAriaValueText={updateHours}
-            step={10}
-            valueLabelDisplay="on"
-            max={200}
-          />
-        </div>
-        <div className="">
-          <label htmlFor="price">Price of product</label>
-          <Slider
-            aria-label="Always visible"
-            defaultValue={20}
-            getAriaValueText={updatePrice}
-            step={1}
-            valueLabelDisplay="on"
-            max={1000}
-          />
-        </div>
-        <p>Antal timmar: </p>
-        <p>{result}</p>
+        <SliderExtended
+          text="Netto månadslön"
+          toolipLabel="Lön efter skatt"
+          sliderSettings={salarySliderSetting}
+          onTextfieldChange={onSalaryChange}
+          textfieldValue={salary}
+          showTextfield={showSalaryTextfield}
+          onToggleTextfiled={onSalaryTextfieldUpdate}
+        />
+        <SliderExtended
+          text="Timmar per månad"
+          sliderSettings={hoursSliderSetting}
+          onTextfieldChange={onHoursChange}
+          textfieldValue={hours}
+          showTextfield={showHoursTextfield}
+          onToggleTextfiled={onHoursTextfieldUpdate}
+        />
+        <SliderExtended
+          text="Pris på produkt"
+          sliderSettings={priceSliderSetting}
+          onTextfieldChange={onPriceChange}
+          textfieldValue={price}
+          showTextfield={showPriceTextfield}
+          onToggleTextfiled={onPriceTextfieldUpdate}
+        />
       </div>
     </main>
   );
