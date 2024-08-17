bef8ce7bc96a3cef44cf54bc891cf69d125c6517
Staff <staffan.solin@gmail.com>
2024-08-17 12:39:18 +0200

    Added Mui-Sliders-still-missing-styling

--- time-vs-salary/src/app/page.tsx
+++ time-vs-salary/src/app/page.tsx
@@ -1,61 +1,75 @@
 "use client";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { Slider } from "@mui/material";
+import { ChangeEvent, useState } from "react";
 
 export default function Home() {
   const [salary, setSalary] = useState(0);
   const [hours, setHours] = useState(0);
   const [price, setPrice] = useState(0);
 
-  function updateSalary(event: ChangeEvent<HTMLInputElement>) {
-    setSalary(+event.target.value);
-  }
-
-  function updateHours(event: ChangeEvent<HTMLInputElement>) {
-    setHours(+event.target.value);
-  }
-
-  function updatePrice(event: ChangeEvent<HTMLInputElement>) {
-    setPrice(+event.target.value);
-  }
-
-  const payPerHour = salary / hours;
-  const timeInHours = price / payPerHour;
-
   function convertDecimalToHoursMinutes(decimalHours: number): string {
     const hours = Math.floor(decimalHours);
     const minutes = Math.round((decimalHours - hours) * 60);
     return `${hours} hours, ${minutes} minutes`;
   }
+
+  const payPerHour = salary / hours;
+  const timeInHours = price / payPerHour;
   const result = convertDecimalToHoursMinutes(timeInHours);
+
+  function updateSalary(value: number) {
+    setSalary(value);
+    return `${value}°C`;
+  }
+
+  function updateHours(value: number) {
+    setHours(value);
+    return `${value}°C`;
+  }
+
+  function updatePrice(value: number) {
+    setPrice(value);
+    return `${value}°C`;
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center  p-24 gap-4">
       <h1>Time vs Salary</h1>
       <div className="flex flex-col gap-2">
-        <div className="flex gap-3">
-          <input
-            type="number"
-            name="salary"
-            onChange={(event) => updateSalary(event)}
-          />
-          <label htmlFor="salary">Monthly salary (after Tax)</label>
+        <div className="flex flex-col gap-3">
+          <div className="">
+            <label htmlFor="salary">Monthly salary (after Tax)</label>
+            <Slider
+              aria-label="Always visible"
+              defaultValue={20000}
+              getAriaValueText={updateSalary}
+              step={100}
+              valueLabelDisplay="on"
+              max={50000}
+            />
+          </div>
         </div>
-
-        <div className="flex gap-3">
-          <input
-            type="number"
-            name="hours"
-            onChange={(event) => updateHours(event)}
-          />
+        <div className="">
           <label htmlFor="hours">Hour working per month</label>
-        </div>
-        <div className="flex gap-3">
-          <input
-            type="number"
-            name="price"
-            onChange={(event) => updatePrice(event)}
+          <Slider
+            aria-label="Always visible"
+            defaultValue={169}
+            getAriaValueText={updateHours}
+            step={10}
+            valueLabelDisplay="on"
+            max={200}
           />
+        </div>
+        <div className="">
           <label htmlFor="price">Price of product</label>
+          <Slider
+            aria-label="Always visible"
+            defaultValue={20}
+            getAriaValueText={updatePrice}
+            step={1}
+            valueLabelDisplay="on"
+            max={1000}
+          />
         </div>
         <p>Antal timmar: </p>
         <p>{result}</p>
